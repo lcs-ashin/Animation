fdbcf38bb061c6b0536d36624961de938623f03b
lcs-ashin <ashin@lcs.on.ca>
2020-10-15 15:30:45 -0400

    I made the circle move to the top right corner using dx

--- Animation/BouncingCircle.swift
+++ Animation/BouncingCircle.swift
@@ -18,6 +18,7 @@ class BouncingCircle: NSObject, Sketchable {
     // Position of circle
     var x: Int
     var y : Int
+    var dx : Int
     
     // This function runs once
     override init() {
@@ -28,19 +29,33 @@ class BouncingCircle: NSObject, Sketchable {
         // Set starting position
         x = 250
         y = 250
+        
+        // Start moving to the right
+        dx = 1
+        
         // Slow down the animation
-        canvas.framesPerSecond = 10
+        canvas.framesPerSecond = 50
         
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
+        
+        
         // Change position
-        x += 1
+        x += dx
         y += 1
         
         // Draw an ellipse in the middle of the canvas
+        let black = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
+        let white = Color(hue: 100, saturation: 0, brightness: 100, alpha: 100)
+     
+        canvas.fillColor = white
+        canvas.drawRectangle(at: Point(x: 0, y: 0), width: 500, height: 500)
+        
+        
+        canvas.fillColor = black
         canvas.drawEllipse(at: Point(x: x, y: y), width: 50, height: 50)
         
     }
