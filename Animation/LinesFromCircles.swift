761204c88839c3d1fbf23e35535e234dc2e60563
lcs-ashin <ashin@lcs.on.ca>
2020-10-17 10:14:14 -0400

    I made the circles to move in different ways and made the line change the colour by the length of it. Then I commented out the white rectangle and circles to let the line draw a shape.

--- Animation/LinesFromCircles.swift
+++ Animation/LinesFromCircles.swift
@@ -25,7 +25,7 @@ class LinesFromCircles: NSObject, Sketchable {
     var n: Int
     var dm: Int
     var dn: Int
-
+    
     
     // This function runs once
     override init() {
@@ -38,16 +38,16 @@ class LinesFromCircles: NSObject, Sketchable {
         let point2 = Int.random(in: 0...500)
         x = point1
         y = point2
-    
+        
         let point3 = Int.random(in: 0...500)
         let point4 = Int.random(in: 0...500)
         m = point3
         n = point4
-
-        dx = 1
-        dy = 1
         
-        dm = 1
+        dx = 1
+        dy = -1
+        
+        dm = -1
         dn = 1
         
         // Slow down the animation
@@ -57,26 +57,26 @@ class LinesFromCircles: NSObject, Sketchable {
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
-      
+        
         // White background
-        let white = Color.init(hue: 100, saturation: 0, brightness: 100, alpha: 100)
-        canvas.drawShapesWithFill = true
-        canvas.drawShapesWithBorders = false
-        canvas.fillColor = white
-
-        canvas.drawRectangle(at: Point(x: 0, y: 0), width: 500, height: 500)
+//        let white = Color.init(hue: 100, saturation: 0, brightness: 100, alpha: 100)
+//        canvas.drawShapesWithFill = true
+//        canvas.drawShapesWithBorders = false
+//        canvas.fillColor = white
+//
+//        canvas.drawRectangle(at: Point(x: 0, y: 0), width: 500, height: 500)
         // Change position
         x += dx
         y += dy
         
         m += dm
         n += dn
-
+        
         // Draw an ellipse in the middle of the canvas
         canvas.drawShapesWithFill = false
         canvas.drawShapesWithBorders = true
         canvas.defaultBorderWidth = 2
-          // Bigger circle
+        // Bigger circle
         if x > 500 {
             dx = -1
         } else if x < 0 {
@@ -86,9 +86,9 @@ class LinesFromCircles: NSObject, Sketchable {
         } else if y < 0 {
             dy = 1
         }
-       
-        canvas.drawEllipse(at: Point(x: x, y: y), width: 250, height: 250)
-        
+
+//        canvas.drawEllipse(at: Point(x: x, y: y), width: 250, height: 250)
+
         // Smaller circle
         if m > 500 {
             dm = -1
@@ -99,13 +99,8 @@ class LinesFromCircles: NSObject, Sketchable {
         } else if n < 0 {
             dn = 1
         }
-     
-      canvas.drawEllipse(at: Point(x: m, y: n), width: 100, height: 100)
-        
-        // Draw a line
-        let purple = Color(hue: 309, saturation: 78, brightness: 64, alpha: 100)
-        canvas.lineColor = purple
-        canvas.defaultLineWidth = 2
+
+//        canvas.drawEllipse(at: Point(x: m, y: n), width: 100, height: 100)
         
         // Calculate the distance between circles
         let horizontal = x - m
@@ -113,10 +108,19 @@ class LinesFromCircles: NSObject, Sketchable {
         
         let d = sqrt(Double(horizontal * horizontal + vertical * vertical))
         
-        if d < 175 {
+        // Map the distance between the circles to a brightness value
+        let brightness = map(value: d, fromLower: 0, fromUpper: 175, toLower: 0, toUpper: 100)
+//        print(brightness)
         
-        canvas.drawLine(from: Point(x: x, y: y), to: Point(x: m, y: n))
-    
+        // Draw a line
+        let purple = Color(hue: 309, saturation: 78, brightness: Int(brightness), alpha: 100)
+        canvas.lineColor = purple
+        canvas.defaultLineWidth = 2
+        
+        if d < 175 {
+            
+            canvas.drawLine(from: Point(x: x, y: y), to: Point(x: m, y: n))
+            
         }
     }
     
