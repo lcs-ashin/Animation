dcfb851fb5d5a5ae600eed975466c6ab016b50c7
lcs-ashin <ashin@lcs.on.ca>
2020-10-17 10:57:18 -0400

    I made the colour, transparency, etc. be random of "LinesFromCircles"

--- Animation/LinesFromCircles.swift
+++ Animation/LinesFromCircles.swift
@@ -44,11 +44,16 @@ class LinesFromCircles: NSObject, Sketchable {
         m = point3
         n = point4
         
+        // Speed of the circles
+        
+       
+        
         dx = 1
         dy = -1
         
-        dm = -1
-        dn = 1
+       
+        dm = -2
+        dn = 2
         
         // Slow down the animation
         canvas.framesPerSecond = 80
@@ -59,13 +64,15 @@ class LinesFromCircles: NSObject, Sketchable {
     func draw() {
         
         // White background
-//        let white = Color.init(hue: 100, saturation: 0, brightness: 100, alpha: 100)
-//        canvas.drawShapesWithFill = true
-//        canvas.drawShapesWithBorders = false
-//        canvas.fillColor = white
-//
-//        canvas.drawRectangle(at: Point(x: 0, y: 0), width: 500, height: 500)
-        // Change position
+        let white = Color.init(hue: 100, saturation: 0, brightness: 100, alpha: 5)
+        canvas.drawShapesWithFill = true
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = white
+
+        if canvas.frameCount % 10 == 0 {
+            canvas.drawRectangle(at: Point(x: 0, y: 0), width: 500, height: 500)
+        }
+         // Change position
         x += dx
         y += dy
         
@@ -78,26 +85,26 @@ class LinesFromCircles: NSObject, Sketchable {
         canvas.defaultBorderWidth = 2
         // Bigger circle
         if x > 500 {
-            dx = -1
+            dx = Int.random(in: 1...3) * -1
         } else if x < 0 {
-            dx = 1
+            dx = Int.random(in: 1...3)
         } else if y > 500 {
-            dy = -1
+            dy = Int.random(in: 1...3) * -1
         } else if y < 0 {
-            dy = 1
+            dy = Int.random(in: 1...3)
         }
 
 //        canvas.drawEllipse(at: Point(x: x, y: y), width: 250, height: 250)
 
         // Smaller circle
         if m > 500 {
-            dm = -1
+            dm = Int.random(in: 1...5) * -1
         } else if m < 0 {
-            dm = 1
+            dm = Int.random(in: 1...5)
         } else if n > 500 {
-            dn = -1
+            dn = Int.random(in: 1...5) * -1
         } else if n < 0 {
-            dn = 1
+            dn = Int.random(in: 1...5)
         }
 
 //        canvas.drawEllipse(at: Point(x: m, y: n), width: 100, height: 100)
@@ -109,15 +116,18 @@ class LinesFromCircles: NSObject, Sketchable {
         let d = sqrt(Double(horizontal * horizontal + vertical * vertical))
         
         // Map the distance between the circles to a brightness value
-        let brightness = map(value: d, fromLower: 0, fromUpper: 175, toLower: 0, toUpper: 100)
+        let value = map(value: d, fromLower: 0, fromUpper: 175, toLower: 200, toUpper: 360)
+        let value2 = map(value: d, fromLower: 0, fromUpper: 175, toLower: 40, toUpper: 80)
+        let saturation = map(value: d, fromLower: 50, fromUpper: 175, toLower: 100, toUpper: 240)
+
 //        print(brightness)
         
         // Draw a line
-        let purple = Color(hue: 309, saturation: 78, brightness: Int(brightness), alpha: 100)
+        let purple = Color(hue: Int(value), saturation: Int(saturation), brightness: 100, alpha: Int(value2))
         canvas.lineColor = purple
-        canvas.defaultLineWidth = 2
+        canvas.defaultLineWidth = 7
         
-        if d < 175 {
+        if d < 200 {
             
             canvas.drawLine(from: Point(x: x, y: y), to: Point(x: m, y: n))
             
