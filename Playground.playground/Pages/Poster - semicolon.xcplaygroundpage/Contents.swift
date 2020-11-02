0c3644e851c755e2287f9ce6c6503650549e8e81
lcs-ashin <ashin@lcs.on.ca>
2020-11-02 09:36:43 -0500

    I add some details such as colours and small texts to "semicolon" poster

--- Playground.playground/Pages/Poster - semicolon.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Poster - semicolon.xcplaygroundpage/Contents.swift
@@ -48,9 +48,12 @@ PlaygroundPage.current.liveView = canvas
 // Colours
 let beige = Color(hue: 14, saturation: 6, brightness: 83, alpha: 100)
 let brown = Color(hue: 14, saturation: 40, brightness: 20, alpha: 100)
-let lightBrown = Color(hue: 15, saturation: 14, brightness: 56, alpha: 65)
 let blue = Color(hue: 222, saturation: 58, brightness: 50, alpha: 100)
 let mustard = Color(hue: 43, saturation: 77, brightness: 80, alpha: 100)
+
+let lightBrown = Color(hue: 15, saturation: 14, brightness: 56, alpha: 65)
+
+
 
 
 // Background
@@ -62,31 +65,52 @@ canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 for x in stride(from: 50, through: 350, by: 100) {
     for y in stride(from: 50, through: 350, by: 100) {
         
-       canvas.fillColor = brown
+        canvas.fillColor = mustard
+        canvas.drawEllipse(at: Point(x: x + 8, y: y + 1), width: 50, height: 50)
+        canvas.lineColor = mustard
+        canvas.defaultLineWidth = 5
+        canvas.drawLine(from: Point(x: x + 29, y: y - 5), to: Point(x: x + 15, y: y - 32))
+
+        canvas.fillColor = blue
+        canvas.drawEllipse(at: Point(x: x + 4, y: y - 1), width: 50, height: 50)
+        canvas.lineColor = blue
+        canvas.drawLine(from: Point(x: x + 25, y: y - 7), to: Point(x: x + 11, y: y - 34))
+        
+        canvas.fillColor = brown
         canvas.drawEllipse(at: Point(x: x, y: y - 3), width: 50, height: 50)
         
         canvas.lineColor = brown
-        canvas.defaultLineWidth = 5
         canvas.drawLine(from: Point(x: x + 21, y: y - 9), to: Point(x: x + 7, y: y - 36))
         
         canvas.fillColor = lightBrown
         canvas.drawEllipse(at: Point(x: x, y: y + 37), width: 50, height: 50)
+       
     }
 }
 
+canvas.lineColor = lightBrown
+canvas.defaultLineWidth = 2
+canvas.drawLine(from: Point(x: 0, y: 420), to: Point(x: 400, y: 420))
+canvas.drawLine(from: Point(x: 0, y: 491), to: Point(x: 400, y: 491))
+canvas.drawLine(from: Point(x: 0, y: 565), to: Point(x: 400, y: 565))
+canvas.drawLine(from: Point(x: 0, y: 547), to: Point(x: 400, y: 547))
+
 // Texts
 canvas.textColor = mustard
-canvas.drawText(message: "Semicolon;", at: Point(x: 29, y: 420), size: 60, kerning: 0)
+canvas.drawText(message: "Semicolon;", at: Point(x: 33, y: 424), size: 60, kerning: 0)
 
 canvas.textColor = blue
-canvas.drawText(message: "Semicolon;", at: Point(x: 27, y: 420), size: 60, kerning: 0)
+canvas.drawText(message: "Semicolon;", at: Point(x: 29, y: 422), size: 60, kerning: 0)
 
 canvas.textColor = brown
 canvas.drawText(message: "Semicolon;", at: Point(x: 25, y: 420), size: 60, kerning: 0)
 
+canvas.drawText(message: "october 19th 6 pm", at: Point(x: 25, y: 550), size: 10, kerning: 0)
+canvas.drawText(message: "special album", at: Point(x: 310, y: 550), size: 10, kerning: 0)
+
 // Grid
-canvas.defaultLineWidth = 1
-canvas.drawAxes(withScale: true, by: 50, color: brown)
+//canvas.defaultLineWidth = 1
+//canvas.drawAxes(withScale: true, by: 50, color: brown)
 
 /*:
  ## Show the Assistant Editor
