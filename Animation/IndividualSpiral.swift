538ffbbf9918d3ae023f9de0dd1573ca2a49682d
lcs-ashin <ashin@lcs.on.ca>
2020-11-09 11:13:04 -0500

    Got the basic outline of a class

---
+++ Animation/IndividualSpiral.swift
@@ -0,0 +1,33 @@
+//
+//  IndividualSpiral.swift
+//  Animation
+//
+//  Created by Eunbi Shin on 2020-11-09.
+//  Copyright © 2020 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+import CanvasGraphics
+
+// Define a class that creates a spiral
+// - a "class" is just a way to group data (properties) together
+//   with behaviour (things that we want to happeen)
+class IndividualSpiral {
+    
+    // 1. Prperties
+    //
+    //    A property is something that descibes the item.
+    //    e.g.: A student at LCS has a house, a hair color, a height
+    
+    
+    // 1. Initializer
+    //
+    //    An initializer has one job: give each property an initial
+    //    value
+    
+    // 3. Methods
+    //
+    // Here we describe what behaviour we want to have happen.
+    
+    
+}
